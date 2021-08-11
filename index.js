5085c022e730d8ac032e1d7e56edc6a3c146edff
Avinash <avinash@localhost.localdomain>
2021-08-11 17:07:48 -0400

    New Version with mocha tests

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 
 app.get('/', function (req, res) {
- res.send('hello world');
+ res.send('hello world from Avinash');
 });//listen to port 3000 by default
 
 //app.listen(process.env.PORT || 3000);
